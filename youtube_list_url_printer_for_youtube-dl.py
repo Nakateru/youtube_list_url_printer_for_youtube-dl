cea7ab76de63e07b14b5ff925080d2bbb0e7d43b
Nakateru <nakateruh@gmail.com>
2020-02-19 21:07:07 +0900

    youtube_list_url_printer_for_youtube-dl

--- youtube_list_url_printer_for_youtube-dl.py
+++ youtube_list_url_printer_for_youtube-dl.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re
+import re,time
 videolist=[]
 resolution='137'
 
@@ -11,7 +11,7 @@ if __name__=='__main__':
     print('Author  :  Nakateru (2020.02.18)')
     url=input('Input Youtube list URL:')
     if url.startswith('https://www.youtube.com/playlist?list='):
-        print('Searching videos')
+        print('Loading')
     else:
         print('Error URL')
         exit()
@@ -25,6 +25,17 @@ if __name__=='__main__':
     wait = WebDriverWait(driver, 10)
     ele = wait.until(EC.element_to_be_clickable((By.ID,'contents')))
     # print(ele)
+
+    last_height = driver.execute_script("return document.documentElement.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
+        time.sleep(1)
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if new_height == last_height:
+            print("Loaded")
+            break
+        last_height = new_height
+
     content = ele.find_elements_by_id('content')
     for i in content:
         videourl=i.find_element_by_tag_name('a').get_attribute('href')
@@ -52,7 +63,7 @@ if __name__=='__main__':
         with open('youtube-dl start list.bat', 'a', encoding='UTF-8') as f:
             for i in videolist:
                 f.write('youtube-dl -f ' + resolution + '+140 ' + i + '\n')
-        print('Save list youtube-dl start list.bat')
+        print('Saved list youtube-dl start list.bat')
     except Exception:
         print('Failed to save list.bat')
         exit()
